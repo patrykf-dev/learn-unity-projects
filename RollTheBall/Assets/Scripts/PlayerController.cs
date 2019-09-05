b1d8840b3f40f473793a5220938006706261308e
gsrmeen <patryk.gsrmeen@gmail.com>
2019-09-05 19:23:52 +0200

    Upload first project

---
+++ RollTheBall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Text countText;
+
+
+    private int pickupsCounter;
+    private Rigidbody rb;
+
+
+    private void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        pickupsCounter = 0;
+        countText.text = "Count: " + pickupsCounter.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    // Update is called before physics calculation
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            pickupsCounter++;
+            countText.text = "Count: " + pickupsCounter.ToString();
+        }
+    }
+}
